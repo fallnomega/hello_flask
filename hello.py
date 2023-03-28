f897ac6e93da20efee1da9e3abb11fba485d6a0f
fallnomega <fallnomega@gmail.com>
2023-03-27 17:49:19 -0700

    messing around with flask and decorators

--- hello.py
+++ hello.py
@@ -1,10 +1,25 @@
 from flask import Flask
+
+# denotes the name of the file currently being run
 app = Flask(__name__)
 
+# python decorator, a decorator is a function thsat gives additional functionality to a function
 @app.route('/')
 def hello_world():
+    #http://127.0.0.1:5000
     return 'Hello World!'
+
+@app.route('/bye')
+def say_bye():
+    #http://127.0.0.1:5000/bye
+    return "Say Goodnight, Gracie!"
+if __name__ == '__main__':
+    app.run()
 
 # be sure to export and run flask app
 # jasonh@local hello_flask > export FLASK_APP=hello.py
 # jasonh@local hello_flask > flask run
+
+
+# decorators
+
